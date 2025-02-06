b2ebff1e685689a0faf828fcf8e3d0825d8256e3
Shigwedha Matheus <shigwedhamatheus13@gmail.com>
2025-02-07 00:32:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*const username = document.getElementById('username');
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+const alert = document.getElementById('alert');
+
+btn.onclick = function(){
+    if (username = "null")
+        alert.textContent = ("Please Enter valid things!");
+    }
+*/
